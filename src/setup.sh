1e58fccfdc33fe6c5db1cbb9e048dcb1c0575569
mlizbeth <mirgantrophy@gmail.com>
2018-10-04 13:05:27 -0500

    eliminate need for raspi-config

--- src/setup.sh
+++ src/setup.sh
@@ -1,3 +1,15 @@
+ln -sf /usr/share/zoneinfo/America/Chicago > /etc/localtime
+sed -i '/en_US.UTF-8 UTF-8/s/^#//g' /etc/locale.gen
+locale-gen &
+wait $PID
+echo "Enter a new password"
+passwd
+echo "Enter the location for this device"
+read loc
+echo $loc > /etc/hostname
+mv keyboard /etc/default/keyboard
+mv wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+sed -i 's/gpu_mem=64/gpu_mem=256/' /boot/config.txt
 apt update
 DEBIAN_FRONTEND=noninteractive apt install -y libgconf-2-4 file-roller thunar gvfs clamav gufw xorg xserver-xorg xserver-xorg-video-fbdev openbox openbox-menu openbox-themes obconf-qt xfce4-terminal chromium-browser &
 wait $PID
@@ -20,25 +32,20 @@ then
 fi
 echo "xset s off\nxset s noblank\nxset -dpms\nsetxkbmap -option terminate:ctrl_alt_bksp" > /home/pi/.config/openbox/autostart
 
-
 if [ ! -e /home/pi/.xinitrc ]
 then
 	touch /home/pi/.xinitrc
 fi
 echo "exec openbox-session" > /home/pi/.xinitrc
 
-
 if [ ! -e /home/pi/.bash_profile ]
 then
 	touch /home/pi/.bash_profile
 fi
-echo "startx -- -nocursor" > /home/pi/.bash_profile
-
-
+echo "startx #-- -nocursor" > /home/pi/.bash_profile
 cd /home/pi
 wget install-versions.risevision.com/installer-lnx-armv7l.sh
 chmod +x installer-lnx-armv7l.sh
-
 echo "  _______     _         _  _          " > /etc/issue
 echo " |__   __|   (_)       (_)| |         " >> /etc/issue
 echo "    | | _ __  _  _ __   _ | |_  _   _ " >> /etc/issue
@@ -47,15 +54,12 @@ echo "    | || |   | || | | || || |_ | |_| |" >> /etc/issue
 echo "    |_||_|   |_||_| |_||_| \__| \__, |" >> /etc/issue
 echo "                                 __/ |" >> /etc/issue
 echo "                                |___/ " >> /etc/issue
-
 echo "______ ___________ _____  ___   _   _ " > /etc/motd
 echo "|  _  \  ___| ___ \_   _|/ _ \ | \ | |" >> /etc/motd
 echo "| | | | |__ | |_/ / | | / /_\ \|  \| |" >> /etc/motd
 echo "| | | |  __|| ___ \ | | |  _  || . \ |" >> /etc/motd
 echo "| |/ /| |___| |_/ /_| |_| | | || |\  |" >> /etc/motd
 echo "|___/ \____/\____/ \___/\_| |_/\_| \_/" >> /etc/motd
-
-                                      
 
 if [ ! -e /etc/systemd/system/getty\@tty1.service.d ]
 then
@@ -67,29 +71,11 @@ then
 	touch /etc/systemd/system/getty\@tty1.service.d/override.conf
 fi
 echo '[Service]\nExecStart=\nExecStart=-/sbin/agetty -a pi --noclear %I $TERM' > /etc/systemd/system/getty@tty1.service.d/override.conf
-
 chown -R pi:users /home/pi
-#touch /var/lib/clamav/clamd.sock
-#chown clamav:clamav /var/lib/clamav/clamd.sock
-#systemctl stop clamav-daemon.service
-freshclam &
-wait $PID
-#systemctl enable clamav-freshclam.service
-#systemctl enable clamav-daemon.service
-
-#mv detected.sh /etc/clamav/
-#sed -i '/ScanOnAccess yes/s/^#//' /etc/clamav/clamd.conf
-#sed -i '/OnAccessMountPath \//s/^#//' /etc/clamav/clamd.conf
-#sed -i '/OnAcessPrevention yes/s/^#//' /etc/clamav/clamd.conf
-#sed -i '/OnAcessExtraScanning yes/s/^#//' /etc/clamav/clamd.conf
-#sed -i 's/User clamav/User root/' /etc/clamav/clamd.conf
-#echo -e "VirusEvent /etc/clamav/detected.sh" >> /etc/clamav/clamd.conf
-
 sed -i 's/#Port 22/Port 13337/' /etc/ssh/sshd_config
 sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
 sed -i 's/#MaxAuthTries 6/MaxAuthTries 3/' /etc/ssh/sshd_config
 sed -i 's/#MaxSessions 10/MaxSessions 2/' /etc/ssh/sshd_config
-
 sed -i 's/gpu_mem=64/gpu_mem=256/' /boot/config.txt
 sed -i 's/disable_overscan=1/#disable_overscan=1' /boot/config.txt
 systemctl enable ssh
